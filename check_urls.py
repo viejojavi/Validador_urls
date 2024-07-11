d3971b8c27cb16276d150d310bbd03e21dd7b3cb
viejojavi <56147006+viejojavi@users.noreply.github.com>
2024-07-11 10:20:19 -0500

    Update check_urls.py

--- check_urls.py
+++ check_urls.py
@@ -1,37 +1,32 @@
 import requests
-from concurrent.futures import ThreadPoolExecutor
 import csv
 
 def check_url(url):
     try:
-        response = requests.head(url, allow_redirects=True, timeout=5)
-        return url, response.status_code
+        response = requests.get(url, timeout=10)
+        return response.status_code
     except requests.RequestException as e:
-        return url, str(e)
+        return str(e)
 
-def check_urls_from_file(file_path):
-    with open(file_path, 'r') as file:
-        urls = [line.strip() for line in file if line.strip()]
-
+def check_urls_from_url(url):
+    response = requests.get(url)
+    urls = response.text.splitlines()
     results = []
-    with ThreadPoolExecutor(max_workers=10) as executor:
-        futures = [executor.submit(check_url, url) for url in urls]
-        for future in futures:
-            results.append(future.result())
-
+    for url in urls:
+        status = check_url(url)
+        results.append((url, status))
     return results
 
 def save_results_to_csv(results, output_file):
     with open(output_file, 'w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(["URL", "Status"])
+        writer.writerow(['URL', 'Status'])
         writer.writerows(results)
 
-if __name__ == "__main__":
-    input_file = 'https://raw.githubusercontent.com/viejojavi/mk/main/listado_urls.txt'  # Archivo con las URLs a verificar
-    output_file = 'results.csv'  # Archivo de salida con los resultados
+input_url = 'https://raw.githubusercontent.com/viejojavi/mk/main/listado_urls.txt'
+output_file = 'results.csv'
 
-    results = check_urls_from_file(input_file)
-    save_results_to_csv(results, output_file)
+results = check_urls_from_url(input_url)
+save_results_to_csv(results, output_file)
 
-    print(f"Resultados guardados en {output_file}")
+print(f"Resultados guardados en {output_file}")
